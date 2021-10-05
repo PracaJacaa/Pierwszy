1edaabd29ab8ad6243c1a17104bfb79ac236529e
Jacek Gipsiak <gipsiak77@gmail.com>
2021-10-05 09:07:18 +0000

    ffirst update

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const first = (
+    <div>
+    <h2>hello world</h2>
+    <p>:)</p>
+    </div>
+);
+ReactDOM.render(first, document.getElementById('Root'));
